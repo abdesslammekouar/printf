9ee9bc986df387a4175b0df992473753936132d1
midnight-error <ilagsoun021@gmail.com>
2023-04-24 17:47:17 -0700

    midnight

--- write_handlers.c
+++ write_handlers.c
@@ -97,7 +97,7 @@ int write_num(int ind, char buffer[],
 	int i, pad_start = 1;
 
 	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0' && width == 0)
-	  return (0);
+		return (0);
 	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
 		buffer[ind] = padd = ' ';
 	if (prec > 0 && prec < length)
