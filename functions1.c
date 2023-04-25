463683e1668c100d656e5d5f3ea7b950770f784d
midnight-error <ilagsoun021@gmail.com>
2023-04-25 08:53:57 -0700

    hot fix

--- functions1.c
+++ functions1.c
@@ -91,7 +91,7 @@ int print_octal(va_list types, char buffer[],
 int print_hexadecimal(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	return (print_hexa(types, "0123456789abc", buffer,
+	return (print_hexa(types, "0123456789abcdef", buffer,
 		flags, 'x', width, precision, size));
 }
 
